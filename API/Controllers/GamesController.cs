9acda532f872cb09460adda8214d9984dca5ca9e
Yarnell Locklear <flocklear@liberty.edu>
2020-04-11 13:45:45 -0400

    Initial Commit

---
+++ API/Controllers/GamesController.cs
@@ -0,0 +1,38 @@
+﻿
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GamesController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public GamesController(StoreContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Game>>> GetGames()
+        {
+            var games = await _context.Games.ToListAsync();
+
+            return Ok(games);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Game>> GetGame(int id)
+        {
+            return await _context.Games.FindAsync(id);
+        }
+    }
+}
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,19 @@
+﻿
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Game> Games { get; set; }
+    }
+}
